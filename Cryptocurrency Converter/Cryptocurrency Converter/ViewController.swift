7808203d2814d1b7e1558c8d9ca67d54ddff9e35
reese <rjkaz@comcast.net>
2021-07-26 15:31:06 -0400

    beginning the crypto app

---
+++ Cryptocurrency Converter/Cryptocurrency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptocurrency Converter
+//
+//  Created by Reese RJ Kaz on 7/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
