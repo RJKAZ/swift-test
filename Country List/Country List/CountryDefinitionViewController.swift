8631c8aeafd09bd745dc6e4883466f798defaa8a
reese <rjkaz@comcast.net>
2021-07-31 15:05:56 -0400

    finished section 6

--- Country List/Country List/CountryDefinitionViewController.swift
+++ Country List/Country List/CountryDefinitionViewController.swift
@@ -13,13 +13,40 @@ class CountryDefinitionViewController: UIViewController {
     
     @IBOutlet weak var countryDefinitionLabel: UILabel!
     
-    var country = "🇦🇽";
+    var country = "🇨🇦";
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         bigFlagLabel.text = country;
-
+        
+        //let flags = ["🇦🇽", "🇦🇷", "🇨🇦", "🇧🇴", "🇮🇨", "🇰🇾"];
+        
+        if country == "🇦🇽"
+        {
+            countryDefinitionLabel.text = "Aland"
+        }
+        if country == "🇦🇷"
+        {
+            countryDefinitionLabel.text = "Argentina"
+        }
+        if country == "🇨🇦"
+        {
+            countryDefinitionLabel.text = "Canada"
+        }
+        if country == "🇧🇴"
+        {
+            countryDefinitionLabel.text = "Bolivia"
+        }
+        if country == "🇮🇨"
+        {
+            countryDefinitionLabel.text = "Canary Islands"
+        }
+        if country == "🇰🇾"
+        {
+            countryDefinitionLabel.text = "New Zealand"
+        }
+        
         
     }
 }
--- Country List/Country List/CountryTableViewController.swift
+++ Country List/Country List/CountryTableViewController.swift
@@ -33,11 +33,14 @@ class CountryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedCountry = flags[indexPath.row];
             
-        performSegue(withIdentifier: "DefinitionSegue", sender: nil)
+        performSegue(withIdentifier: "DefinitionSegue", sender: selectedCountry)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let defOfViewController = segue.destination as! CountryDefinitionViewController;
-        defOfViewController.country = "";
+        let selectedCountry = sender as! String;
+        
+        
+        defOfViewController.country = selectedCountry;
     }
     
     // left off at 11:20
