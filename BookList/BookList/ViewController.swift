6668cb914dee5fc041322a67a6007db93c669fed
reese <rjkaz@comcast.net>
2021-08-22 23:18:15 -0400

    guard, else, and do

--- BookList/BookList/ViewController.swift
+++ BookList/BookList/ViewController.swift
@@ -9,6 +9,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // we will write a function that will take our book data from the internet
+    
+    func fetchBookData(completionHandler: @escaping([Book]) -> Void){
+        let bookListURL =
+        "https://reeses-blog-default-rtdb.firebaseio.com/"
+        
+        guard let url = URL(string: bookListURL) else { return }
+        
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { ( data, response, error) in
+            do {
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode([Book].self, from: data)
+            }
+            catch{
+                let error = error
+                print(error.localizedDescription)
+            }
+        }
+                       
+    }
 }
 
